ef390b48b46ba5245295dcb2734e85a5bec531fe
Kalyani Vuttnoor <vuttnoor.kalyani@gmail.com>
2025-12-10 19:28:23 +0530

    sshagent added

--- server-script.sh
+++ server-script.sh
@@ -1,8 +1,8 @@
-# sudo yum install java -y
+sudo yum install java -y
 sudo yum install git -y
-# sudo yum install maven -y
-sudo yum install docker -y
-sudo service docker start
+ sudo yum install maven -y
+#sudo yum install docker -y
+#sudo service docker start
 
 
 if [ -d "addressbook-v1" ]
@@ -15,6 +15,6 @@ else
 fi
 
 cd /home/ec2-user/addressbook-v1
-#mvn package
+mvn package
 
-sudo docker build -t $1 .
+#sudo docker build -t $1 .
